875da797f3470cb7a77d6d9aa7700f10838eca41
fakt309 <43887554+fakt309@users.noreply.github.com>
2020-06-09 10:33:09 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -217,13 +217,10 @@ var decryptHolder = function(data) {
   return decryptedHolder;
 };
 
-function testUser(decryptedUser, encryptedUser, socketid) {
+function testUser(decryptedUser, encryptedUser) {
   if (decryptedUser.id == encryptedUser.id && decryptedUser.email == encryptedUser.email && decryptedUser.dateSignup == encryptedUser.dateSignup) {
-    var holders = encryptedUser.split('!!!!!2');
+    var holders = encryptedUser.holders.split('!!!!!2');
     for (var i = 0; i < holders.length; i++) {
-      io.to(socketid).emit('sendtextttt', decryptHolder(holders[i]));
-      io.to(socketid).emit('sendtextttt', decryptHolder(decryptedUser.holders[i]));
-      io.to(socketid).emit('sendtextttt', "----------------------");
       if (decryptHolder(holders[i]) != decryptedUser.holders[i]) {
         return false;
       }
@@ -1115,7 +1112,7 @@ io.sockets.on('connection', function(socket) {
       connection.query("SELECT * FROM users WHERE id='"+validUser.id+"'", function (err, result, fields) {
         io.to(socket.id).emit('sendtextttt', validUser);
         io.to(socket.id).emit('sendtextttt', result[0]);
-        io.to(socket.id).emit('sendtextttt', testUser(validUser, result[0], socket.id));
+        io.to(socket.id).emit('sendtextttt', testUser(validUser, result[0]));
         if (result[0] && testUser(validUser, result[0])) {
           io.to(socket.id).emit('sendtextttt', result[0]);
           var pstTime = new Date(Date.now()+new Date().getTimezoneOffset()*60*1000+(-7*60*60*1000));
