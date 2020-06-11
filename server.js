e667ba82b9d4fb48f20375cbe0f835f660580fd5
fakt309 <43887554+fakt309@users.noreply.github.com>
2020-06-11 10:21:38 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1186,8 +1186,6 @@ io.sockets.on('connection', function(socket) {
   });
 
   socket.on('getDataStudio', function(name, user) {
-    io.to(socket.id).emit('sendtextttt', name);
-    io.to(socket.id).emit('sendtextttt', user);
     var connection = mysql.createConnection({
       host: "vh50.timeweb.ru",
       user: "totarget_gmerio",
@@ -1195,10 +1193,13 @@ io.sockets.on('connection', function(socket) {
       database: "totarget_gmerio"
     });
     connection.connect(function(err) {
-      connection.query("SELECT * FROM studios WHERE name='"+name+"'", function (err1, result1, fields1) {
+      connection.query("SELECT * FROM studios WHERE LOWER(name)=LOWER('"+name+"')", function (err1, result1, fields1) {
+        io.to(socket.id).emit('sendtextttt', result1[0]);
         if (result1[0]) {
           if (user && user.id) {
             connection.query("SELECT * FROM users WHERE id='"+user.id+"'", function (err2, result2, fields2) {
+              io.to(socket.id).emit('sendtextttt', result2[0]);
+              io.to(socket.id).emit('sendtextttt', testUser(user, result2[0]));
               if (result2[0] && testUser(user, result2[0])) {
                 var flagKeyHolder = false;
                 var studiosUser = result2[0].studios.split(',');
